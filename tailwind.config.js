6dc2804c791582eef26231aafca329fd62dffcf5
Varshith-p <varshithpuligadda@gmail.com>
2023-01-09 20:11:43 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.js", "./components/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
